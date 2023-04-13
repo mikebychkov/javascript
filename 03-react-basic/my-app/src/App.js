cf73477a89b533afaf0d4decd9befd6c4994ead5
Mike <mihailbichkov@yandex.ru>
2023-04-13 18:02:35 +0500

    component basics

--- 03-react-basic/my-app/src/App.js
+++ 03-react-basic/my-app/src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Component, StrictMode} from 'react';
+
+const Header = () => {
+  return <h2>Hello World!</h2>  
+}
+
+const Field = () => {
+  const holder = 'Type here';
+  const styledField = {
+    width: '300px'
+  };
+  return <input 
+          type="text" 
+          placeholder={holder} 
+          style={styledField} />
+}
+
+// class FieldByClass extends React.Component {
+class FieldByClass extends Component {
+  render() {
+    const holder = 'Type here';
+    const styledField = {
+      width: '300px'
+    };
+    return <input 
+            type="text" 
+            placeholder={holder} 
+            style={styledField} />
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  return <button>{text}</button>
+}
+
+function Btn1() {
+  const textFunc = () => 'Log in';
+  return <button>{textFunc()}</button>
+}
+
+function Btn2() {
+  const textElem = <p>Log in</p>;
+  return <button>{textElem}</button>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StrictMode>
+        <Header/>
+      </StrictMode>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- 03-react-basic/my-app/src/index.js
+++ 03-react-basic/my-app/src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello World!</h2>; // JSX
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!'); // NATIVE JS
-
-const someText = 'Some text';
-
-const elem2 = (
-  <div>
-    <h2 className="text-class">Hello World!</h2>
-    <label htmlFor="in">Here:</label>
-    <input id="in" type="text"/>
-    <button tabIndex={0}>Click</button>
-    <h3>{someText}</h3>
-  </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  elem2
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
