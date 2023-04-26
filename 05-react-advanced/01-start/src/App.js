6a34071c3aa3a34454826e9fd48d8b4af2ca949f
Mike <mihailbichkov@yandex.ru>
2023-04-26 23:51:27 +0500

    react hooks

--- 05-react-advanced/01-start/src/App.js
+++ 05-react-advanced/01-start/src/App.js
@@ -1,6 +1,8 @@
-import {Component, useState} from 'react';
+import {Component, useState, useEffect, useMemo, useCallback, useRef} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Slider extends Component {
 
@@ -10,6 +12,14 @@ class Slider extends Component {
             autoplay: false,
             slide: 0
         }
+    }
+
+    componentDidMount() {
+        document.title = `Slide #${this.state.slide}`;
+    }
+
+    componentDidUpdate() {
+        document.title = `Slide #${this.state.slide}`;
     }
 
     changeSlide = (i) => {
@@ -27,7 +37,7 @@ class Slider extends Component {
     render() {
         return (
             <Container>
-                <div className="slider w-50 m-auto">
+                <div className="slider w-100 m-auto">
                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
                     <div className="buttons mt-3">
@@ -47,7 +57,11 @@ class Slider extends Component {
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 const FSlider = (props) => {
+
+    // STATE
 
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
@@ -60,10 +74,102 @@ const FSlider = (props) => {
         setAutoplay(autoplay => !autoplay);
     }
 
+    // EFFECT
+
+    const logging = () => {
+        console.log('logging ' + new Date());
+    }
+
+    useEffect(() => {
+        console.log('effect on init AND update slide state');
+        document.title = `Slide #${slide}`;
+    }, [slide]);
+
+    useEffect(() => {
+        console.log('effect on init ONLY (componentDidMount analog)');
+
+        window.addEventListener('click', logging);
+
+        return () => {                                       // RETURNING FUNCTION IS ANALOG TO 'componentWillUnmount'
+            window.removeEventListener('click', logging);
+        };
+
+    }, []);
+
+    // MEMO
+
+    const autoplayText = useMemo(() => {
+        return autoplay ? 'auto' : null;
+    }, [autoplay]);
+
+
+    // REF
+
+    const myRef = useRef(null);
+
+    const onPicClick = () => {
+        myRef.current.focus();
+    };
+
     return (
         <Container>
-            <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+            <div className="slider w-100 m-auto">
+                <img onClick={onPicClick} className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                {/* <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div> */}
+                <div className="text-center mt-5">Active slide {slide} <br/> {autoplayText}</div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+                <div>
+                    <input ref={myRef} className="" placeholder="Input here"/>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+const FSlider2 = (props) => {
+
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
+
+    const changeSlide = (i) => {
+        setSlide(slide => slide + i);
+    }
+    
+    const toggleAutoplay = () => {
+        setAutoplay(autoplay => !autoplay);
+    }
+
+    // CALLBACK
+
+    const getNewImages = useCallback(() => {
+
+        console.log('IMAGE REQUEST');
+
+        return [
+            'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+            'https://www.agoda.com/wp-content/uploads/2020/07/Statue-of-Liberty-things-to-do-in-new-york-USA.jpg'
+        ];
+    }, [slide]);
+
+
+    return (
+        <Container>
+            <div className="slider w-100 m-auto">
+                
+                <Slide getNewImages={getNewImages}/>
+                
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
@@ -81,11 +187,59 @@ const FSlider = (props) => {
     )
 }
 
+const Slide = ({getNewImages}) => {
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getNewImages());
+    }, [getNewImages]);
+
+    return images.map((img, i) => {
+        return <img key={i} className="d-block w-100" src={img} alt="slide" />;
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+const Msg = (props) => {
+
+    return (
+        <div style={{'textAlign': 'center'}}>
+            <h2>{props.msg}</h2>
+        </div>
+    );
+}
+
 function App() {
-  return (
+ 
+    const [sliderOn, setSliderOn] = useState(true);
+ 
+    const onToggleSlider = () => {
+        console.log('TOGGLE SLIDER');
+        setSliderOn(sliderOn => !sliderOn);
+    };
+
+    return (
     <>
-        {/* <Slider/> */}
-        <FSlider/>
+        <div className="row">
+            <div className="col">
+                <Msg msg="Class type Component:"/>
+                <Slider/>
+            </div>
+            <div className="col">
+                <Msg msg="Function type Component:"/>
+                <button className="btn btn-primary m-2" onClick={onToggleSlider}>Toggle slider</button>
+                {sliderOn ? <FSlider/> : null}
+            </div>
+        </div>
+        <hr/>
+        <div className="row">
+            <div className="col"/>
+            <div className="col">
+                <FSlider2/>
+            </div>
+        </div>
     </>
   );
 }
