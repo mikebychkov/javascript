e2db7e119f173ad387c9b32e012ea837ec8056b1
Mike <mihailbichkov@yandex.ru>
2023-05-18 17:30:44 +0500

    formik use on contact-me form

--- my-back/src/main/java/service/ServiceApplication.java
+++ my-back/src/main/java/service/ServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceApplication {
 
+	// TODO:
+	// - front-end - FORMIK
+	// - front-end - data-service
+	// - https://www.baeldung.com/spring-email
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
--- my-site/src/components/contact-me/contact-me.js
+++ my-site/src/components/contact-me/contact-me.js
@@ -1,25 +1,48 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 import './contact-me.css';
 
 const ContactMe = () => {
 
-	const onSubmit = e => {
-		e.preventDefault();
-		console.log('submitting email message');
+	const formik = useFormik({
+        initialValues: {
+			email: '',
+			emailmessage: ''
+		},
+		validationSchema: yup.object({
+			email: yup.string()
+						.required('Required field')
+						.email('Invalid e-mail address format'),
+			emailmessage: yup.string()
+						.required('Required field')
+						.min(100, 'Minimum 100 symbols')
+						.max(1000, 'Maximum 1000 symbols')
+		}),
+		onSubmit: values => onSubmit(JSON.stringify(values, null, 2))
+	});
+
+	const onSubmit = body => {
+		console.log('submitting email message:', body);
 	}
+
+	const fargs = fieldArgs(formik);
 
 	return (
 		<div className="contact-me">
 			<a href="#contact-me" id="contact-me"><h2>Contact Me</h2></a>
 			<div className="decor"></div>
 
-			<form onSubmit={onSubmit}>
+			<form onSubmit={formik.handleSubmit}>
 
 				<div className="form-group">
-					<input type="email" className="form-control" placeholder="Enter email" id="email" name="email"/>
+					<input className="form-control" placeholder="Enter email" { ...fargs('email', 'email') }/>
+					<ValidationError name="email" formik={formik}/>
 				</div>
 				<div className="form-group">
-					<textarea rows="10" className="form-control" placeholder="Enter message" id="emailmessage" name="emailmessage">
+					<textarea rows="10" className="form-control" placeholder="Enter message" { ...fargs('emailmessage', 'string') } >
 					</textarea> 
+					<ValidationError name="emailmessage" formik={formik}/>
 				</div>
 				<button type="submit" className="btn btn-light">Send Message</button>                    
 
@@ -27,6 +50,25 @@ const ContactMe = () => {
 
 		</div>
 	);
+}
+
+const ValidationError = ({name, formik}) => {
+
+    return (
+        formik.errors[name] && formik.touched[name] ? <div className="error">{formik.errors[name]}</div> : null
+    );
+}
+
+const fieldArgs = (formik) => {
+
+    return (name, type) => ({
+        id: name,
+        name: name,
+        type: type,
+        value: formik.values[name],
+        onChange: formik.handleChange,
+        onBlur: formik.handleBlur,
+    });
 }
 
 export default ContactMe;
