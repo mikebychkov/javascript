67acb01cadc7c120466d44ba40e700cfc30da29e
Mike <mihailbichkov@yandex.ru>
2023-06-06 03:50:37 +0500

    todos update

--- my-back/src/main/java/service/ServiceApplication.java
+++ my-back/src/main/java/service/ServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceApplication {
 
+	// TODO: HEALTH-CHECK ENDPOINT TO SUPPLY TO CONTAINER ORCHESTRATOR
 	// TODO: RETRIEVE AND SEND EMAILS BY SCHEDULE
 	// - https://www.baeldung.com/spring-email
 
