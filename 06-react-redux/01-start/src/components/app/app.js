9f66c8b54940c2234f6fee12f421556aad288671
Mike <mihailbichkov@yandex.ru>
2023-06-25 13:06:52 +0500

    start with redux

---
+++ 06-react-redux/01-start/src/components/app/app.js
@@ -0,0 +1,15 @@
+import './app.css';
+
+const App = () => {
+
+    return (
+        <div className="jumbotron">
+            {/* <h1>0</h1>
+            <button class="btn btn-primary">INC</button>
+            <button class="btn btn-primary">DEC</button>
+            <button class="btn btn-primary">RND</button> */}
+        </div>
+    );
+}
+
+export default App;
---
+++ 06-react-redux/01-start/src/js/reducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+    value: 0
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC': return {...state, value: state.value + 1};
+        case 'DEC': return {...state, value: state.value - 1};
+        case 'RND': return {...state, value: action.payload * state.value};
+        default: return state;
+    }
+}
+
+export default reducer;
